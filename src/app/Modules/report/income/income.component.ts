16e241d9d4e70cca7295d83f020eef1eb4b21b9f
Arold Ayson <aroldayson3@gmail.com>
2024-08-01 18:15:03 -0700

    update report route

--- src/app/Modules/report/income/income.component.ts
+++ src/app/Modules/report/income/income.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-income',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './income.component.html',
   styleUrl: './income.component.css'
 })
