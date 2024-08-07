79871f3c11099c2fb89715f1b1f3aaa20a13d232
Arold Ayson <aroldayson3@gmail.com>
2024-08-07 15:04:21 -0700

    update grid

--- src/app/Modules/price-managemt/add-price/add-price.component.ts
+++ src/app/Modules/price-managemt/add-price/add-price.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-price',
@@ -9,5 +10,22 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './add-price.component.css'
 })
 export class AddPriceComponent {
+
+  save(){
+    Swal.fire({
+      title: "Are you sure to add categoty?",
+      showDenyButton: true,
+      // showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  }
 
 }
