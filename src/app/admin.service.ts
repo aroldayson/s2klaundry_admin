cb7cbd49693e70bf5b2fa4f983a74c61e227ed24
Arold Ayson <aroldayson3@gmail.com>
2024-08-26 18:51:31 -0700

    updatetodate

---
+++ src/app/admin.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  url = "http://localhost/admin/";
+
+  constructor(private http: HttpClient) { }
+
+  addprice(data: any){
+    return this.http.post(this.url + 'addprice',JSON.stringify(data));
+  }
+  displayitem(){
+    return this.http.get(this.url + 'displayprice.php');
+  }
+}
